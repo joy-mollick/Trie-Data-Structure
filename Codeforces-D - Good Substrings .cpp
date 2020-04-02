51683d048f5c52e66915e2c9bca788d31d0d1083
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2020-04-02 02:07:42 -0700

    Update Codeforces-D - Good Substrings .cpp

--- Codeforces-D - Good Substrings .cpp
+++ Codeforces-D - Good Substrings .cpp
@@ -2,44 +2,51 @@
 
     using namespace std;
 
-    const int ALPHABET_SIZE = 2;
 
     char good[26];
-    int bd=0;
+   struct TrieNode
+   {
+       struct TrieNode *children[26];
+   };
+   struct TrieNode *getNode(void)
+    {
+        struct TrieNode *pNode =  new TrieNode;
 
-   int trie[1500*1500][26];
+        for (int i = 0; i < 26; i++)
+            pNode->children[i] = NULL;
 
+        return pNode;
+    }
     int main()
     {
+        struct TrieNode *root=getNode();
         char s[1500];
         scanf("%s",s);
         scanf("%s",good);
         int k;scanf("%d",&k);
         int len=strlen(s);
-        string temp="";
         int ans=0;
-        int pref=1;
+        int pref=0;
         for(int i=0;i<len;i++)
         {
             int occur=0;
-            int cur=1;/// start from root =1 node
+            struct TrieNode *curr=root;/// every time from root , new string will start 
             for(int j=i;j<len;j++)
             {
                 if(good[s[j]-'a']=='0') occur++;
                 if(occur>k) {break;}/// bad characters exceed k
-                if(!trie[cur][s[j]-'a'])
+                if(!curr->children[s[j]-'a'])/// if there is no children , make it 
                 {
-                    pref++;/// number of prefix increased as well as size of trie
-                    trie[cur][s[j]-'a']=pref;
-                    cur=pref;/// now every character goes up to this trie indexing pref.
+                    pref++;/// number of distinct prefix of substrings 
+                    curr->children[s[j]-'a']=getNode();
+                    curr=curr->children[s[j]-'a'];
                 }
                 else
                 {
-                    cur=trie[cur][s[j]-'a'];
+                    curr=curr->children[s[j]-'a'];
                 }
             }
         }
-        pref--;/// prefix is one increment for root , so decrease one , it will count now number of distinct substrings 
         printf("%d\n",pref);
         return 0;
     }
